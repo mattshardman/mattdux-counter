65cceccd72d89cc9b244f2f77f7d2e6ce947e882
mattshardman <mattshardman@gmail.com>
2019-06-19 15:31:34 +0100

    create counter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
-     
+     <div>
+       <Counter />
+     </div>
     );
   }
 }
