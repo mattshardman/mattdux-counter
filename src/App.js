e7fe2aaeb284514b0ff6d9bf0092242788fcae6f
mattshardman <mattshardman@gmail.com>
2019-06-19 15:50:52 +0100

    style counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from "styled-components";
+
 import Counter from './Counter';
 
-class App extends Component {
-  render() {
-    return (
-     <div>
-       <Counter />
-     </div>
-    );
-  }
+const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f9f9f9;
+`;
+
+function App() {
+  return (
+    <Main>
+      <Counter />
+    </Main>
+  )
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.section`
+  box-sizing: border-box;
+  width: 250px;
+  height: 200px;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 8px;
+  border: 1px solid #dadce0;
+  background: #fff;
+  box-shadow: 0 4px 20px rgba(0,0,0,0.1);
+`;
+
+const Screen = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #dadce0;
+  border-radius: 4px;
+  background: rgb(232, 240, 254);
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+
+  button {
+    width: 80px;
+    height: 30px;
+    background: #1a73e8;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+  }
+`;
 
 function Counter() {
-    return (
-        <div>
-            counter!!
-        </div>
-    );
+  return (
+    <Container>
+      <Screen><h1>counter!!</h1></Screen>
+      <Buttons>
+        <button>+</button>
+        <button>-</button>
+      </Buttons>
+    </Container>
+  );
 }
 
 export default Counter;
