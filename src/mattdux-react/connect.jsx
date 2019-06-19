81c325e9560486be068ab8887ffa33d1ae4293fb
mattshardman <mattshardman@gmail.com>
2019-06-19 18:51:59 +0100

    add actionsReducer and state to connect function

--- src/mattdux-react/connect.jsx
+++ src/mattdux-react/connect.jsx
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Context } from '.';
 
 const connect = (mapStateToProps, actions) => (Component) => {
   function Connect(props) {
     const mattdux = useContext(Context);
-    console.log(actions);
+    const [state, setState] = useState();
+    const [actionFunctions, setActionFunctions] = useState();
+
+    useEffect(() => {
+      setState(mattdux.store);
+
+      const actionsArr = Object.entries(actions);
+      const funcs = actionsArr.reduce((acc, [actionName, actionFunc]) => {
+        acc[actionName] = (input) => {
+          const payload = actionFunc(input);
+          const newState = mattdux.callReducers(payload);
+          setState(newState);
+        };
+        return acc;
+      }, {});
+
+      setActionFunctions(funcs);
+    }, []);
+
     return (
-      <Component {...props} />
+      <Component {...props} {...actionFunctions} {...state} />
     );
   }
 
