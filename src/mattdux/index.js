6ed8ce95b34dce69396c8710efa5255712867c3e
mattshardman <mattshardman@gmail.com>
2019-06-19 16:55:21 +0100

    add createStore method to Mattdux

--- src/mattdux/index.js
+++ src/mattdux/index.js
@@ -4,9 +4,21 @@ class Mattdux {
     this.actions = [];
     this.store = {};
   }
+
+  createStore = (reducers) => {
+    const reducerArr = Object.entries(reducers);
+
+    reducerArr.forEach(([reducerName, reducer]) => {
+      this.reducers = { ...this.reducers, [reducerName]: reducer };
+
+      const initialState = reducer(undefined, { type: 'INIT' });
+      this.store = { ...this.store, [reducerName]: initialState };
+    });
+  }
 }
 
-export function createStore() {
+export function createStore(reducers) {
   const mattdux = new Mattdux();
+  mattdux.createStore(reducers);
   return mattdux;
 }
