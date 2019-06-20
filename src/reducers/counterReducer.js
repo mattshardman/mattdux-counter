7c1fbe5991c9f2be7e840c4f03dec2c3d4150140
mattshardman <mattshardman@gmail.com>
2019-06-20 08:12:59 +0100

    change state format

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,9 +1,9 @@
-export default (state = 0, action) => {
+export default (state = { num: 0 }, action) => {
   switch (action.type) {
     case 'ADD':
-      return state + 1;
+      return { num: state.num + 1 };
     case 'SUB':
-      return state - 1;
+      return { num: state.num - 1 };
     default:
       return state;
   }
